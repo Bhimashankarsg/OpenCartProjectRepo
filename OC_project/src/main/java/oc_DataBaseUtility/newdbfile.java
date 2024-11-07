b9aff9574dfe21deb36399cead17b89d11c750f3
Shankar S G <Shankar S G@DESKTOP-OQCH81E.mshome.net>
2024-11-07 09:57:10 +0530

    Merge branch 'OC_Branch_6.0.0' of https://github.com/Bhimashankarsg/OpenCartProjectRepo.git

--- OC_project/src/main/java/oc_DataBaseUtility/newdbfile.java
+++ OC_project/src/main/java/oc_DataBaseUtility/newdbfile.java
@@ -3,6 +3,6 @@ package oc_DataBaseUtility;
 public class newdbfile {
 public static void main()
 {
-	System.out.println("Hello from AL LEAD d");
+	System.out.println("Hello from AL LEAD d BRANCH");
 }
 }
